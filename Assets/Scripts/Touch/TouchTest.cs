b08612db05c557762de173cdcf20222de12779c1
Rebeca Sarai <rebecasaraiguair@gmail.com>
2017-01-27 02:12:23 -0200

    adds script for get input information

---
+++ Assets/Scripts/Touch/TouchTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void getInfoTouch(){
+		Touch touch = Input.GetTouch (0);
+		//Moviment direction of the touch
+		Vector2 delta = touch.deltaPosition;
+		Debug.Log (delta);
+	}
+}
